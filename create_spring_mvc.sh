f4a74312e7a33944c56fae0e433e9ec2b8d25a7b
liviutudor <me@liviutudor.com>
2013-08-27 19:04:00 -0700

    added more stuff
    compiles, haven't tried to run yet

--- create_spring_mvc.sh
+++ create_spring_mvc.sh
@@ -18,140 +18,234 @@ GROUP_ID="liviutudor"
 PKG_NAME="liv"
 
 POM_XML="
-<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
-	xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd\">
-	<modelVersion>4.0.0</modelVersion>
-	<groupId>$GROUP_ID</groupId>
-	<artifactId>$PRJ_NAME</artifactId>
-	<packaging>war</packaging>
-	<version>${PRJ_VERSION}-SNAPSHOT</version>
-	<name>$PRJ_NAME</name>
-	<inceptionYear>$CURR_YEAR</inceptionYear>
-	<description>$PRJ_DESC</description>
-	<url>$PRJ_URL</url>
-	<developers>
-		<developer>
-			<name>Liviu Tudor</name>
-			<id>liviut</id>
-			<email>me at liviutudor.com</email>
-		</developer>
-	</developers>
-	<repositories>
-		<repository>
-			<snapshots>
-				<enabled>false</enabled>
-			</snapshots>
-			<id>central</id>
-			<name>Maven Repository Switchboard</name>
-			<url>http://repo1.maven.org/maven2</url>
-		</repository>
-	</repositories>
-
-	<properties>
-		<project.build.jdkVersion>1.6</project.build.jdkVersion>
-		<spring.version>3.0.6.RELEASE</spring.version>
-	</properties>
-	<dependencies>
-		<dependency>
-			<groupId>junit</groupId>
-			<artifactId>junit</artifactId>
-			<version>4.8.1</version>
-			<scope>test</scope>
-		</dependency>
-		<dependency>
-			<groupId>javax.servlet</groupId>
-			<artifactId>servlet-api</artifactId>
-			<version>2.5</version>
-			<scope>provided</scope>
-		</dependency>
-		<dependency>
-			<groupId>javax.servlet</groupId>
-			<artifactId>jstl</artifactId>
-			<version>1.2</version>
-		</dependency>
-		<dependency>
-			<groupId>org.springframework</groupId>
-			<artifactId>spring-core</artifactId>
-			<version>\${spring.version}</version>
-		</dependency>
-		<dependency>
-			<groupId>org.springframework</groupId>
-			<artifactId>spring-jdbc</artifactId>
-			<version>\${spring.version}</version>
-		</dependency>
-		<dependency>
-			<groupId>org.springframework</groupId>
-			<artifactId>spring-webmvc</artifactId>
-			<version>\${spring.version}</version>
-		</dependency>
-		<dependency>
-			<groupId>org.springframework</groupId>
-			<artifactId>spring-context</artifactId>
-			<version>\${spring.version}</version>
-		</dependency>
-		<dependency>
-			<groupId>org.springframework</groupId>
-			<artifactId>spring-test</artifactId>
-			<version>\${spring.version}</version>
-			<scope>test</scope>
-		</dependency>
-	</dependencies>
-
-	<build>
-		<finalName>$PRJ_NAME</finalName>
-		<defaultGoal>install</defaultGoal>
-		<plugins>
-			<plugin>
-				<artifactId>maven-compiler-plugin</artifactId>
-				<configuration>
-					<source>\${project.build.jdkVersion}</source>
-					<target>\${project.build.jdkVersion}</target>
-				</configuration>
-			</plugin>
-		</plugins>
-	</build>
+<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n
+	xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd\">\n
+	<modelVersion>4.0.0</modelVersion>\n
+	<groupId>$GROUP_ID</groupId>\n
+	<artifactId>$PRJ_NAME</artifactId>\n
+	<packaging>war</packaging>\n
+	<version>${PRJ_VERSION}-SNAPSHOT</version>\n
+	<name>$PRJ_NAME</name>\n
+	<inceptionYear>$CURR_YEAR</inceptionYear>\n
+	<description>$PRJ_DESC</description>\n
+	<url>$PRJ_URL</url>\n
+	<developers>\n
+		<developer>\n
+			<name>Liviu Tudor</name>\n
+			<id>liviut</id>\n
+			<email>me at liviutudor.com</email>\n
+		</developer>\n
+	</developers>\n
+	<repositories>\n
+		<repository>\n
+			<snapshots>\n
+				<enabled>false</enabled>\n
+			</snapshots>\n
+			<id>central</id>\n
+			<name>Maven Repository Switchboard</name>\n
+			<url>http://repo1.maven.org/maven2</url>\n
+		</repository>\n
+	</repositories>\n
+	\n
+	<properties>\n
+		<project.build.jdkVersion>1.6</project.build.jdkVersion>\n
+		<spring.version>3.0.6.RELEASE</spring.version>\n
+	</properties>\n
+	\n
+	<dependencies>\n
+		<dependency>\n
+			<groupId>junit</groupId>\n
+			<artifactId>junit</artifactId>\n
+			<version>4.8.1</version>\n
+			<scope>test</scope>\n
+		</dependency>\n
+		<dependency>\n
+			<groupId>javax.servlet</groupId>\n
+			<artifactId>servlet-api</artifactId>\n
+			<version>2.5</version>\n
+			<scope>provided</scope>\n
+		</dependency>\n
+		<dependency>\n
+			<groupId>javax.servlet</groupId>\n
+			<artifactId>jstl</artifactId>\n
+			<version>1.2</version>\n
+		</dependency>\n
+		<dependency>\n
+			<groupId>org.springframework</groupId>\n
+			<artifactId>spring-core</artifactId>\n
+			<version>\${spring.version}</version>\n
+		</dependency>\n
+		<dependency>\n
+			<groupId>org.springframework</groupId>\n
+			<artifactId>spring-jdbc</artifactId>\n
+			<version>\${spring.version}</version>\n
+		</dependency>\n
+		<dependency>\n
+			<groupId>org.springframework</groupId>\n
+			<artifactId>spring-webmvc</artifactId>\n
+			<version>\${spring.version}</version>\n
+		</dependency>\n
+		<dependency>\n
+			<groupId>org.springframework</groupId>\n
+			<artifactId>spring-context</artifactId>\n
+			<version>\${spring.version}</version>\n
+		</dependency>\n
+		<dependency>\n
+			<groupId>org.springframework</groupId>\n
+			<artifactId>spring-test</artifactId>\n
+			<version>\${spring.version}</version>\n
+			<scope>test</scope>\n
+		</dependency>\n
+	</dependencies>\n
+	\n
+	<build>\n
+		<finalName>$PRJ_NAME</finalName>\n
+		<defaultGoal>install</defaultGoal>\n
+		<plugins>\n
+			<plugin>\n
+				<artifactId>maven-compiler-plugin</artifactId>\n
+				<configuration>\n
+					<source>\${project.build.jdkVersion}</source>\n
+					<target>\${project.build.jdkVersion}</target>\n
+				</configuration>\n
+			</plugin>\n
+		</plugins>\n
+	</build>\n
 </project>
 "
 
 WEB_XML="
-<!DOCTYPE web-app PUBLIC \"-//Sun Microsystems, Inc.//DTD Web Application 2.3//EN\" \"http://java.sun.com/dtd/web-app_2_3.dtd\">
-<web-app>
-  <display-name>$PRJ_NAME</display-name>
-     <servlet>
-        <servlet-name>$PRJ_NAME</servlet-name>
-        <servlet-class>org.springframework.web.servlet.DispatcherServlet</servlet-class>
-        <load-on-startup>1</load-on-startup>
-    </servlet>
-
-    <servlet-mapping>
-        <servlet-name>$PRJ_NAME</servlet-name>
-        <url-pattern>/</url-pattern>
-    </servlet-mapping>
-
-    <!-- allow robots.txt -->
-    <servlet-mapping>
-        <servlet-name>default</servlet-name>
-        <url-pattern>*.txt</url-pattern>
-    </servlet-mapping>
-    
-    <!-- allow favicon.ico -->
-    <servlet-mapping>
-        <servlet-name>default</servlet-name>
-        <url-pattern>*.ico</url-pattern>
-    </servlet-mapping>
-
-    <!-- allow everything under /img/ -->
-    <servlet-mapping>
-        <servlet-name>default</servlet-name>
-        <url-pattern>/img/*</url-pattern>
-    </servlet-mapping>
-
-    <!-- allow everything under css -->
-    <servlet-mapping>
-        <servlet-name>default</servlet-name>
-        <url-pattern>/css/*</url-pattern>
-    </servlet-mapping>
+<!DOCTYPE web-app PUBLIC \"-//Sun Microsystems, Inc.//DTD Web Application 2.3//EN\" \"http://java.sun.com/dtd/web-app_2_3.dtd\">\n
+<web-app>\n
+  <display-name>$PRJ_NAME</display-name>\n
+     <servlet>\n
+        <servlet-name>$PRJ_NAME</servlet-name>\n
+        <servlet-class>org.springframework.web.servlet.DispatcherServlet</servlet-class>\n
+        <load-on-startup>1</load-on-startup>\n
+    </servlet>\n
+    \n
+    <servlet-mapping>\n
+        <servlet-name>$PRJ_NAME</servlet-name>\n
+        <url-pattern>/</url-pattern>\n
+    </servlet-mapping>\n
+    \n
+    <!-- allow robots.txt -->\n
+    <servlet-mapping>\n
+        <servlet-name>default</servlet-name>\n
+        <url-pattern>*.txt</url-pattern>\n
+    </servlet-mapping>\n
+    \n
+    <!-- allow favicon.ico -->\n
+    <servlet-mapping>\n
+        <servlet-name>default</servlet-name>\n
+        <url-pattern>*.ico</url-pattern>\n
+    </servlet-mapping>\n
+    \n
+    <!-- allow everything under /img/ -->\n
+    <servlet-mapping>\n
+        <servlet-name>default</servlet-name>\n
+        <url-pattern>/img/*</url-pattern>\n
+    </servlet-mapping>\n
+    \n
+    <!-- allow everything under css -->\n
+    <servlet-mapping>\n
+        <servlet-name>default</servlet-name>\n
+        <url-pattern>/css/*</url-pattern>\n
+    </servlet-mapping>\n
 </web-app>
+"
+
+SERVLET_XML="
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n
+<beans xmlns=\"http://www.springframework.org/schema/beans\"\n
+       xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n
+       xmlns:p=\"http://www.springframework.org/schema/p\"\n
+       xmlns:context=\"http://www.springframework.org/schema/context\"\n
+       xsi:schemaLocation=\"\n
+        http://www.springframework.org/schema/beans\n
+        http://www.springframework.org/schema/beans/spring-beans-3.0.xsd\n
+        http://www.springframework.org/schema/context\n
+        http://www.springframework.org/schema/context/spring-context-3.0.xsd\">\n
+\n
+    <context:component-scan base-package=\"$PKG_NAME\"/>\n
+\n
+    <bean id=\"viewResolver\" class=\"org.springframework.web.servlet.view.UrlBasedViewResolver\">\n
+        <property name=\"viewClass\" value=\"org.springframework.web.servlet.view.JstlView\"/>\n
+        <property name=\"prefix\" value=\"/WEB-INF/jsp/\"/>\n
+        <property name=\"suffix\" value=\".jsp\"/>\n
+    </bean>\n
+\n
+</beans>
+"
+
+APP_CONTEXT_XML="
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n
+<beans xmlns=\"http://www.springframework.org/schema/beans\"\n
+        xmlns:context=\"http://www.springframework.org/schema/context\"\n
+        xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n
+        xsi:schemaLocation=\"http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans-3.0.xsd \n
+       http://www.springframework.org/schema/context http://www.springframework.org/schema/context/spring-context-3.0.xsd\">\n
+\n
+        <bean\n
+                class=\"org.springframework.beans.factory.config.PropertyPlaceholderConfigurer\">\n
+                <property name=\"locations\">\n
+                        <list>\n
+                                <value>classpath*:config/*.properties</value>\n
+                        </list>\n
+                </property>\n
+                <property name=\"ignoreResourceNotFound\" value=\"true\" />\n
+        </bean>\n
+</beans>
+"
+
+PAGE_JSP="
+<%@ taglib uri=\"http://java.sun.com/jstl/core\" prefix=\"c\"%>\n
+<%@page import=\"$PKG_NAME.*\"%>\n
+<jsp:useBean id=\"param_name\" scope=\"request\" class=\"java.lang.String\" />\n
+<html>\n
+<head>\n
+<title>$PRJ_NAME</title>\n
+</head>\n
+<body>\n
+<p>Hello, <c:out value=\"\${param_name}\"/>!</p>\n
+</body>\n
+</html>
+"
+
+ROBOTS_TXT="
+# Disallow robots to index any part of our contents\n
+User-agent: *\n
+Disallow: /\n
+"
+
+INDEX_JSP="
+<%\n
+response.sendRedirect( \"/home\");\n
+%>
+"
+
+HOME_JAVA="
+package $PKG_NAME;\n
+\n
+import java.io.IOException;\n
+import java.util.List;\n
+\n
+import javax.servlet.http.HttpServletRequest;\n
+import javax.servlet.http.HttpServletResponse;\n
+import javax.servlet.http.HttpSession;\n
+\n
+import org.springframework.stereotype.Controller;\n
+import org.springframework.web.bind.annotation.RequestMapping;\n
+import org.springframework.web.bind.annotation.RequestMethod;\n
+import org.springframework.web.servlet.ModelAndView;\n
+\n
+@Controller\n
+public class HomeController {\n
+    @RequestMapping(value = \"/home\", method = RequestMethod.GET)\n
+    public ModelAndView home(HttpSession session, HttpServletResponse response) throws IOException {\n
+        return new ModelAndView(\"home\", \"param_name\", \"world\");\n
+    }\n
+}
 "
 
 # Top level dir
@@ -170,16 +264,37 @@ cd main
 # src/main/java
 mkdir java
 cd java 
+# src/main/java/package
+mkdir $PKG_NAME
+cd $PKG_NAME
+echo -e $HOME_JAVA > HomeController.java
+cd ..
 
 cd ..
 # src/main/resources
 mkdir resources
+cd resources
+# src/main/resources/config
+mkdir config
+cd config
+touch $PRJ_NAME.properties
+cd ..
+cd ..
 # src/main/webapp
 mkdir webapp
 cd webapp
+echo -e $ROBOTS_TXT > robots.txt
+echo -e $INDEX_JSP > index.jsp
 mkdir WEB-INF
 cd WEB-INF
-echo $WEB_XML > web.xml
+echo -e $WEB_XML > web.xml
+SERVLET_FILE="$PRJ_NAME-servlet.xml"
+echo -e $SERVLET_XML > $SERVLET_FILE
+echo -e $APP_CONTEXT_XML > appicationContext.xml
+mkdir jsp
+cd jsp
+echo -e $PAGE_JSP > page.jsp
+cd ..
 cd ..
 cd ..
 cd ..
